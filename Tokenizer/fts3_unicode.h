2cfb18b7e208d5ef0831d8e78927bbb9260c6764
Manoj <Manoj@Manojs-Mac-mini.local>
2014-03-12 16:11:24 +0530

    - token extractions

--- Tokenizer/fts3_unicode.h
+++ Tokenizer/fts3_unicode.h
@@ -18,3 +18,5 @@ int sqlite3FtsUnicodeIsalnum(int c);
 int sqlite3FtsUnicodeIsdiacritic(int c);
 int sqlite3FtsUnicodeFold(int c, int bRemoveDiacritic);
 
+int unicodeTokens(char* utf);
+
--- Tokenizer/main.m
+++ Tokenizer/main.m
@@ -21,6 +21,13 @@ int main(int argc, const char * argv[])
         NSLog(@"Hello, World! - START");
 
         Test();
+        
+//        बीबीसी हिंदी डॉटकॉम पर भारत, पाकिस्तान और चीन सहित 
+//        NSString *uni2=[[NSString alloc] initWithUTF8String:"दुनिया भर की ताज़ा ख़बरें"];
+        NSString *uni2=[[NSString alloc] initWithUTF8String:"Esto es manoj, poniendo a prueba este código"];
+        
+        unicodeTokens([uni2 cStringUsingEncoding:NSUTF8StringEncoding]);
+        
         NSLog(@"Hello, World! - END");
     }
     return 0;
